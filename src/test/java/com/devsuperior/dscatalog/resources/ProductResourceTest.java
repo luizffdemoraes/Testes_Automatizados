88ea64dec03c6844ddf82d05bbc7923f03b38b85
Luiz Moraes <lffm1994@gmail.com>
2022-01-07 01:27:51 -0300

    [02-31] - Legibilidade e negociação de conteúdo

--- src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
+++ src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
@@ -41,8 +43,11 @@ class ProductResourceTest {
 
     @Test
     public void findAllShouldReturnPage() throws Exception {
-        mockMvc.perform(get("/products"))
-                .andExpect(status().isOk());
+       ResultActions resultActions =
+               mockMvc.perform(get("/products")
+                       .accept(MediaType.APPLICATION_JSON));
+       
+       resultActions.andExpect(status().isOk());
     }
 
 }
